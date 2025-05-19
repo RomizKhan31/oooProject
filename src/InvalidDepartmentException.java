b2ada52fd1bb8dda186752885cb11e943d31bb2e
RomizKhan31 <romizkhan313@gmail.com>
2025-05-19 14:18:15 +0600

    add exception

---
+++ src/InvalidDepartmentException.java
@@ -0,0 +1,6 @@
+package Exception;
+public class InvalidDepartmentException extends Exception {
+    public InvalidDepartmentException(String message) {
+        super(message);
+    }
+}
---
+++ src/MyException.java
@@ -0,0 +1,6 @@
+package Exception;
+public class MyException extends Exception {
+    public MyException(String m) {
+        super(m);
+    }
+}
